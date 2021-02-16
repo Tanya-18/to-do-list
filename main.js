860352f752847520ac4ccf558389d45acb9fb467
tanya <tanya.sabarwal2001@gmail.com>
2021-02-16 13:25:44 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let addTodoButton = document.getElementById('addTodo');
+let todocontainer = document.getElementById('todocontainer');
+let inputField = document.getElementById('inputField');
+
+addTodoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    todocontainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+
+})
